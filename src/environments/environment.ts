ea84cc1ad98993c75c2c7ddb29435c2c384291ed
mahan-noor <ismahanabey@gmail.com>
2021-04-03 11:12:00 +0300

    add Api url in evironment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  githubApi:"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"
 };
 
 /*
