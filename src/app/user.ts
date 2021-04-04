737859a2f17abff7beb7308bc6cf5b4ebacde2bb
mahan-noor <ismahanabey@gmail.com>
2021-04-04 09:45:43 +0300

    this will add constructor in class user

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,4 @@
-export class User {
+export class User { 
+   constructor(public name: string, public html_url: string, public login: any, public created_at: any, public repos: number, public following: number, public followers:number, public avatar_url: any, public homepage: string) {} 
+
 }
